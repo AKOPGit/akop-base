9c7a7308934b20b78387174a326a4e70c96b05fa
Aleksandr Kopelevitch <contato@akop.com.br>
2025-07-26 11:36:55 -0300

    Update rich-text-editor.blade.php

--- resources/views/components/forms/rich-text-editor.blade.php
+++ resources/views/components/forms/rich-text-editor.blade.php
@@ -44,6 +44,6 @@
         <button type="button" data-command="undo" class="toolbar-item">↶</button>
         <button type="button" data-command="redo" class="toolbar-item">↷</button>
     </div>
-    <div class="editor p-2 prose"></div>
+    <div class="editor px-4 prose prose-zinc prose-sm prose-a:text-blue-600"></div>
 </div>
 <input type="hidden" name="{{ $name }}" value="{{ $value }}" data-editor-target="{{ $id }}">
