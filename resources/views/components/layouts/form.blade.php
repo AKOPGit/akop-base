49225031f765438c5ebc0c06c1c939d8f3eb2e8f
Aleksandr Kopelevitch <contato@akop.com.br>
2025-07-31 20:53:56 -0300

    Favicon and InterVariable font added

--- resources/views/components/layouts/form.blade.php
+++ resources/views/components/layouts/form.blade.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <title>{{ $title }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="icon" href="{{ asset('img/favicon/favicon.ico') }}" sizes="any">
+    <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}">
+    <meta name="theme-color" content="#ffffff">
 </head>
 <body class="p-6">
     <div class="mx-auto max-w-4xl">
